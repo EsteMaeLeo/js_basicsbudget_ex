3d1ba965f895f2721e09fc5b13c4ca47c1bc44e3
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-04-05 22:43:38 -0500

    new changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-const incomes = [new Income("salario", 2100), new Income("Car sale", 1500)];
+const incomes = [new Income("Salary", 2100), new Income("Car sale", 1500)];
 
 const expenses = [new Expenses("rent", 900), new Expenses("tickets", 90)];
 
 let loadApp = () => {
   loadCabecero();
+  loadIncomes();
 };
 
 let totalIncomes = () => {
@@ -25,13 +26,14 @@ let totalExpenses = () => {
 let loadCabecero = () => {
   let totalInco = totalIncomes();
   let buget = totalIncomes() - totalExpenses();
-  let porcentage = (totalExpenses() / totalIncomes()) ;
+  let porcentage = totalExpenses() / totalIncomes();
   document.getElementById("budget").innerHTML = formatCurrency(buget);
   document.getElementById("income").innerHTML = formatCurrency(totalIncomes());
   document.getElementById("expense").innerHTML = formatCurrency(
     totalExpenses()
   );
-  document.getElementById("porcentage").innerHTML = formatPorcentage(porcentage);
+  document.getElementById("porcentage").innerHTML =
+    formatPorcentage(porcentage);
 };
 
 const formatCurrency = (valor) => {
@@ -53,4 +55,28 @@ const formatPorcentage = (valor) => {
     style: "percent",
     minimumFractionDigits: 2,
   });
+};
+
+const loadIncomes = () => {
+  let incomesHTML = " ";
+  for (let income of incomes) {
+    incomesHTML += createIncomesHTML(income);
+  }
+  document.getElementById("lista-ingresos").innerHTML = incomesHTML;
+};
+
+const createIncomesHTML = (income) => {
+  let incomeHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${income.description}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">${formatCurrency(income.value)}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"></ion-icon>
+            </button>
+        </div>
+    </div>
+  </div>`;
+  return incomeHTML;
 };
